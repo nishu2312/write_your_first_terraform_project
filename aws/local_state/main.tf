1a02b57a225feb5cdeac96dcc4ab1b498376fb6e
Nishant <94179583+nishu2312@users.noreply.github.com>
2025-03-21 14:50:04 +0000

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-000810356197b9b0d"
+  ami           = "ami-00bb6a80f01f03502
   instance_type = "t2.micro"
 
   tags = {
