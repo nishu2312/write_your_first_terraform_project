bb649835cd306937b3f0eba136d682932a1a4f61
Nishant <94179583+nishu2312@users.noreply.github.com>
2025-03-24 16:51:05 +0000

    Update main.tf

--- aws/remote_state/main.tf
+++ aws/remote_state/main.tf
@@ -1,15 +1,13 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # CREATE AN S3 BUCKET AND DYNAMODB TABLE TO USE AS A TERRAFORM BACKEND
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # ----------------------------------------------------------------------------------------------------------------------
 # REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
-# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
-# This module is forked from https://github.com/gruntwork-io/intro-to-terraform/tree/master/s3-backend
 # ----------------------------------------------------------------------------------------------------------------------
 
 terraform {
-    required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
@@ -18,59 +16,61 @@ terraform {
 
   required_version = ">= 1.11.2"
 
-backend "s3" {
-    encrypt = true
+  backend "s3" {
+    bucket         = "588738580614-terraform-states"
+    key            = "development/service-name.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
-
 }
 
-bucket  = "588738580614-terraform-states"
-key     = "development/service-name.tfstate"
-encrypt = true
-region  = "ap-south-1"
-dynamodb_table = "terraform-lock"
-
-# ------------------------------------------------------------------------------
-# CONFIGURE OUR AWS CONNECTION
-# ------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# CONFIGURE AWS PROVIDER
+# ----------------------------------------------------------------------------
 
 provider "aws" {
-  region  = "ap-south-1"
+  region = "ap-south-1"
 }
 
-# ------------------------------------------------------------------------------
-# CREATE THE S3 BUCKET
-# ------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# FETCH AWS ACCOUNT ID
+# ----------------------------------------------------------------------------
 
 data "aws_caller_identity" "current" {}
 
 locals {
-  account_id    = data.aws_caller_identity.current.account_id
+  account_id = data.aws_caller_identity.current.account_id
 }
 
+# ----------------------------------------------------------------------------
+# CREATE THE S3 BUCKET
+# ----------------------------------------------------------------------------
+
 resource "aws_s3_bucket" "terraform_state" {
-  # With account id, this S3 bucket names can be *globally* unique.
   bucket = "${local.account_id}-terraform-states"
+}
 
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
 
-# ------------------------------------------------------------------------------
-# CREATE THE DYNAMODB TABLE
-# ------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# CREATE THE DYNAMODB TABLE FOR STATE LOCKING
+# ----------------------------------------------------------------------------
 
 resource "aws_dynamodb_table" "terraform_lock" {
   name         = "terraform-lock"
